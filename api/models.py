9e9529775f38b9e62c27ce08918511c840e40064
SakibSibly <sibly@duck.com>
2025-03-10 12:59:28 +0600

    Model for the LULUCOIN Block added

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class LuluCoinBlock(models.Model):
+    block_hash = models.TextField()
+    previous_block_hash = models.TextField()
+    transactions = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.block_hash
