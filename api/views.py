1690a8a005893a8dca883d9890870e3770fa74d5
SakibSibly <sibly@duck.com>
2025-03-10 16:25:12 +0600

    Permission for block adding is granted only for the Admin

--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ import hashlib
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import LuluCoinBlock
 from .serializers import LuluCoinBlockSerializer
 from decouple import config
@@ -23,7 +23,7 @@ class InfoView(APIView):
 
 
 class AddBlockView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAdminUser]
     def post(self, request):
         serializer = LuluCoinBlockSerializer(data=request.data)
 
