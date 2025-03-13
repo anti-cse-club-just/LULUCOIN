5c6116f62c8b9252ede2fb9c05dafcecb30369f0
SakibSibly <sibly@duck.com>
2025-03-13 09:44:47 +0600

    URLs for global variable check, registration, transactions added

--- api/urls.py
+++ api/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('info/', views.InfoView.as_view(), name='intro'),
     path('token/', TokenObtainPairView.as_view(), name='get-token'),
     path('token/refresh/', TokenRefreshView.as_view(), name='refresh-token'),
+    path('global-variables/', views.GlobalVariablesView.as_view(), name='global-variables-adjustments'),
+    path('register/', views.CustomRegister.as_view(), name='register'),
+    path('transactions/', views.TransactionView.as_view(), name="transaction-hadling"),
     path('add-block/', views.AddBlockView.as_view(), name='add-new-block'),
     path('validate-blockchain/', views.ValidateView.as_view(), name='validate-bolckchain'),
 ]
