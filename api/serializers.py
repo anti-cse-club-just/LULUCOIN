c9a729665f3e143fe07f5d006ad6265fbb99258a
SakibSibly <sibly@duck.com>
2025-03-13 09:47:11 +0600

    Basic serializer for the DataModel implemented

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,26 @@
 from rest_framework import serializers
-from .models import LuluCoinBlock
+from .models import LuluCoinBlock, Transaction, CustomUser, GlobalVariables
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('first_name', 'last_name', 'username', 'email', 'city', 'password')
 
 
 class LuluCoinBlockSerializer(serializers.ModelSerializer):
     class Meta:
         model = LuluCoinBlock
-        fields = ('transactions', 'created_by')
-        
+        fields = ('block_hash', 'previous_block_hash', 'transactions', 'timestamp', 'nonce', 'difficulty', 'created_by')
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = ('sender', 'receiver', 'amount', 'signature')
+
+
+class GlobalVariablesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GlobalVariables
+        fields = ('difficulty', 'mining_reward', 'fees')
